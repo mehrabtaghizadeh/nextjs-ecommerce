08d64edc6e7317dd85035aae4cfbcc8af9db5ab5
Mehrab <mehrabt999@gmail.com>
2024-01-17 09:13:25 -0800

    2 commit

--- src/utils/BASE_URL.ts
+++ src/utils/BASE_URL.ts
@@ -1,3 +1,4 @@
-const BASE_URL = 'http://localhost:4000/api/v1'
+// const BASE_URL = 'http://localhost:4000/api/v1'
+const BASE_URL = "https://ecommerce-api-qlti.onrender.com/api/v1"
 
 export default BASE_URL
